b33a5ddc6536cb0cae373dbd99c0a5c31a27f7bf
amanmakwana-4 <ma5066770@gmail.com>
2025-12-15 18:20:29 +0530

    fix cors credentials issue

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,6 @@ app.use(cors({
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
 }));
 
 app.options("*", cors());
